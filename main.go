c9b937415773643233e4c2fa9b4885f5a1ed8f1c
Gediminas Z <gediminas.zubovicius@gmail.com>
2024-10-27 15:17:46 +0000

    Starting to define the algorithm

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+type Node struct {
+	name  string
+	edges []*Node
+}
+
+func (n *Node) addEdge(e *Node) {
+	n.edges = append(n.edges, e)
+}
+
+func resolveDependancies(n *Node) {
+	fmt.Println(n.name)
+	for _, edge := range n.edges {
+		resolveDependancies(edge)
+	}
+}
+
+func main() {
+	a := Node{name: "A"}
+	b := Node{name: "B"}
+	c := Node{name: "C"}
+	d := Node{name: "D"}
+	e := Node{name: "E"}
+
+	a.addEdge(&b)
+	a.addEdge(&d)
+	b.addEdge(&c)
+	b.addEdge(&e)
+	c.addEdge(&d)
+	c.addEdge(&e)
+
+	resolveDependancies(&a)
 }
